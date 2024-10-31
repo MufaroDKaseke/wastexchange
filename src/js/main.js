d30e56cce7ce2b7a8646bec086c17518514cadca
MufaroDKaseke <mufarodkaseke@gmail.com>
2024-10-31 07:18:50 +0200

    Added netlify.toml

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 
+// DOM elements
 const preloader = $('.preloader');
+const header = $('#header');
 // Prelosder
 $(window).on('load', () => {
   preloader.fadeOut('slow', () => {
@@ -11,12 +13,21 @@ $(window).scroll(function() {
   // Any scroll events and animations here
   const circleBg = $('#circleBg');
   const circleBg2 = $('#circleBg2');
-  if ($(this).scrollTop() > 250) {
+  if ($(this).scrollTop() > 150) {
+    header.addClass('header-bg');
     circleBg.fadeOut('slow', () => {
       circleBg.css('opacity', 0);
     });
     circleBg2.fadeOut('slow', () => {
       circleBg2.css('opacity', 0);
+    });
+  } else {
+    header.removeClass('header-bg');
+    circleBg.fadeIn('slow', () => {
+      circleBg.css('opacity', 1);
+    });
+    circleBg2.fadeIn('slow', () => {
+      circleBg2.css('opacity', 1);
     });
   }
 });
